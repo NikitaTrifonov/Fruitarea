e1cd845a0afdbbc7091222647b2df5c889b15bf7
NikitaTrifonov <55878043+NikitaTrifonov@users.noreply.github.com>
2019-10-08 15:55:14 +0300

    Клонирвоание пошло
    Клонирование каточек.  Баг с странной производителем.

--- script1.js
+++ script1.js
@@ -1,29 +1,38 @@
-/*let productsData = [
-    {
-        imgUrl ='assets/images/orang1.jpg',
-        nameProduct = 'Апельсин'
-    },
-    {
-        imgUrl ='assets/images/orang1.jpg',
-        nameProduct = 'Апельсин'
-    },
-    {
-        imgUrl ='assets/images/orang1.jpg',
-        nameProduct = 'Апельсин'
-    }
-];
-
-*/
-
-let bascetButton = document.querySelector('.bascet_button');
-let countPieceBox = document.querySelector('.count_box');
-let favoriteButton = document.querySelector('.favorite_button');
-let inputCount = countPieceBox.querySelector('.input_count');
+let bascetButton = document.querySelector('.bascet_button')
+let countPieceBox = document.querySelector('.count_box')
+let favoriteButton = document.querySelector('.favorite_button')
+let inputCount = countPieceBox.querySelector('.input_count')
 let minusCountButton = countPieceBox.querySelector('.minus_button')
 let plusCountButton = countPieceBox.querySelector('.plus_button')
-/*let productItemTemplate = document.querySelector('#product_item_template').content;
-let newProductItem = productItemTemplate.querySelector('.product_item');
-let productList = document.querySelector('.products');*/
+
+let productItemTemplate = document.querySelector('#product_card_template').content;
+let newProductItem = productItemTemplate.querySelector('.product_wrapper');
+let productList = document.querySelector('.products');
+let productsData = [
+    {
+        imgUrl: 'assets/images/orang1.jpg',
+        nameProduct: 'Апельсин',
+        producingСountry: 'Уругвай',
+        averageWeight: '1 шт. ~ 0.1 кг.',
+        shelf_life: '4 мес.'
+
+
+    },
+    {
+        imgUrl: 'assets/images/banan1.jpg',
+        nameProduct: 'Банан',
+        producingСountry: 'Перу',
+        averageWeight: '1 шт. ~ 0.2 кг.',
+        shelf_life: '3 мес.'
+    },
+    {
+        imgUrl: 'assets/images/mango1.jpg',
+        nameProduct: 'Манго',
+        producingСountry: 'Колумбия',
+        averageWeight: '1 шт. ~ 0.3 кг.',
+        shelf_life: '4 мес.'
+    }
+]
 
 
 bascetButton.addEventListener('click', function () {
@@ -55,7 +64,7 @@ minusCountButton.addEventListener('click', function () {
 })
 
 plusCountButton.addEventListener('click', function () {
-    if (inputCount.value ==20) {
+    if (inputCount.value == 20) {
         inputCount.value = 20;
     }
     else {
@@ -63,14 +72,27 @@ plusCountButton.addEventListener('click', function () {
     }
 })
 
+productsData.forEach(function(productData){
+    let newItem = newProductItem.cloneNode(true);
+    let img = newItem.querySelector('#product_img');
+    img.setAttribute('src', productData.imgUrl);
+    img.setAttribute('alt', productData.nameProduct);
 
+    let name = newItem.querySelector('#product_name');
+    name.textContent = productData.nameProduct;
+
+    let producingСountry = newProductItem.querySelector('#country');
+    producingСountry.textContent = productData.producingСountry;
+
+    let averageWeight = newItem.querySelector('#average_weight');
+    averageWeight.textContent = productData.averageWeight;
+
+
+
+
+    productList.appendChild(newItem);
+})
 
 /*for (let i = 0; i < productsData.length; i++) {
-    let newItem = newProductItem.cloneNode(true);
-    var img = newItem.querySelector('#product_img');
-    img.style.src = productsData[i].imgUrl;
-    img.style.alt = productsData[i].nameProduct;
-    var name = newItem.querySelector('#product_name');
-    name.text() = productsData[i].nameProduct;
-    productList.appendChild(newItem);
+    
 }*/
