e88c3fd625c97a23c8b092953db6d1ad57736d91
NikitaTrifonov <55878043+NikitaTrifonov@users.noreply.github.com>
2019-10-09 09:02:25 +0300

    исправил баг стран

--- script1.js
+++ script1.js
@@ -11,26 +11,24 @@ let productList = document.querySelector('.products');
 let productsData = [
     {
         imgUrl: 'assets/images/orang1.jpg',
-        nameProduct: 'Апельсин',
-        producingСountry: 'Уругвай',
-        averageWeight: '1 шт. ~ 0.1 кг.',
-        shelf_life: '4 мес.'
-
-
+        nameProduct:'Апельсин',
+        producingСountry:'Уругвай',
+        averageWeight:'1 шт. ~ 0.1 кг.',
+        shelf_life:'1 мес.'
     },
     {
         imgUrl: 'assets/images/banan1.jpg',
-        nameProduct: 'Банан',
-        producingСountry: 'Перу',
-        averageWeight: '1 шт. ~ 0.2 кг.',
-        shelf_life: '3 мес.'
+        nameProduct:'Банан',
+        producingСountry:'Перу',
+        averageWeight:'1 шт. ~ 0.2 кг.',
+        shelf_life:'2 мес.'
     },
     {
         imgUrl: 'assets/images/mango1.jpg',
-        nameProduct: 'Манго',
-        producingСountry: 'Колумбия',
-        averageWeight: '1 шт. ~ 0.3 кг.',
-        shelf_life: '4 мес.'
+        nameProduct:'Манго',
+        producingСountry:'Колумбия',
+        averageWeight:'1 шт. ~ 0.3 кг.',
+        shelf_life:'3 мес.'
     }
 ]
 
@@ -82,11 +80,14 @@ productsData.forEach(function(productData){
     let ItemName = newItem.querySelector('#product_name');
     ItemName.textContent = productData.nameProduct;
 
-    let prodСountry = newProductItem.querySelector('#country');
-    prodСountry.textContent = productData.producingСountry;
+    let country = newItem.querySelector('#country');
+    country.textContent = productData.producingСountry;
 
     let averageWeight = newItem.querySelector('#average_weight');
     averageWeight.textContent = productData.averageWeight;
+
+    let shelfLife = newItem.querySelector('#shelf_life');
+    shelfLife.textContent = productData.shelf_life;
 
 
 
